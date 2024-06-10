12bf07dd02ec810916d78f1e4389d51d3e85c11a
chaelim <eoett88@gmail.com>
2024-06-10 09:45:07 +0900

    자격증 등록, 닉네임으로 조회, 삭제 구현 완료

--- src/main/java/org/ict/intelligentclass/certificate/jpa/repository/MyCertificateRepository.java
+++ src/main/java/org/ict/intelligentclass/certificate/jpa/repository/MyCertificateRepository.java
@@ -2,21 +2,15 @@ package org.ict.intelligentclass.certificate.jpa.repository;
 
 
 import org.ict.intelligentclass.certificate.jpa.entity.MyCertificateEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-
+import java.util.List;
 
 
 @Repository
 public interface MyCertificateRepository extends JpaRepository<MyCertificateEntity, String> {
 
-    Page<MyCertificateEntity> findByNickname(String nickname,  Pageable pageable);
-
-
+    List<MyCertificateEntity> findByNicknameContaining(String nickname);
 
 }
