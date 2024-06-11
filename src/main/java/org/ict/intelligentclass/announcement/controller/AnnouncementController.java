bb2da8ee4c84626c07415257642edc7380a22a85
허강 <phreshoutof@gmail.com>
2024-06-11 18:26:49 +0900

    공지사항 찐막

--- src/main/java/org/ict/intelligentclass/announcement/controller/AnnouncementController.java
+++ src/main/java/org/ict/intelligentclass/announcement/controller/AnnouncementController.java
@@ -23,35 +23,29 @@ public class AnnouncementController {
 
     @GetMapping("/id")
     public ResponseEntity<AnnouncementEntity> getOneAnnouncement(@RequestParam int announcementId) {
-        log.info(String.valueOf(announcementId));
         AnnouncementEntity entity = announcementService.selectOneAnnouncement(announcementId);
         return ResponseEntity.ok(entity);
     }
 
     @GetMapping
     public ResponseEntity<List<AnnouncementDto>> getAnnouncements(@RequestParam int page){
-        log.info("this happened");
         ArrayList<AnnouncementDto> list = announcementService.selectAnnouncements(page);
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/categorized")
     public ResponseEntity<List<AnnouncementEntity>> getCategorizedAnnouncements(@RequestParam int page, @RequestParam long category){
-        log.info("page: " + page + "category: " + category);
-        System.out.println("page: " + page + "category: " + category);
         List<AnnouncementEntity> list = announcementService.selectCategorizedAnnouncements(page, category);
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<AnnouncementEntity>> getAnnouncementsByKeyword(@RequestParam int page, @RequestParam String keyword){
-
         List<AnnouncementEntity> list = announcementService.selectAnnouncementsByKeyword(page, keyword);
         return ResponseEntity.ok(list);
     }
     @PostMapping
     public ResponseEntity<AnnouncementEntity> insertAnnouncement(@RequestBody AnnouncementEntity announcementEntity){
-        log.info(announcementEntity.toString());
         AnnouncementEntity entity = announcementService.insertAnnouncement(announcementEntity);
         return new ResponseEntity<>(entity, HttpStatus.CREATED);
 
@@ -59,7 +53,6 @@ public class AnnouncementController {
 
     @PutMapping
     public ResponseEntity<AnnouncementEntity> updateAnnouncement(@RequestBody AnnouncementEntity announcementEntity){
-        log.info(announcementEntity.toString());
         AnnouncementEntity entity = announcementService.updateAnnouncement(announcementEntity);
         return new ResponseEntity<>(entity, HttpStatus.ACCEPTED);
 
@@ -67,7 +60,6 @@ public class AnnouncementController {
 
     @DeleteMapping
     public ResponseEntity<Void> deleteAnnouncement(@RequestBody AnnouncementEntity announcementEntity){
-        log.info(announcementEntity.getAnnouncementId().toString());
         announcementService.deleteAnnouncement(announcementEntity.getAnnouncementId());
         return new ResponseEntity<>(HttpStatus.OK);
     }
