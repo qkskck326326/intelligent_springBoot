2034ccee26618e619507e754ad2ac4b319a90fa1
rudalsdl <lll0803lll@naver.com>
2024-06-13 18:26:32 +0900

    lecture 패키지입니다.

--- src/main/java/org/ict/intelligentclass/lecture/jpa/repository/LectureRepository.java
+++ src/main/java/org/ict/intelligentclass/lecture/jpa/repository/LectureRepository.java
@@ -9,16 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface LectureRepository
     extends JpaRepository<LectureEntity, Integer> {
 
-
-
-//    LecturePackageEntity findById(Long lecturePackageId);
-
-
-//    List<LectureEntity> findByLecturePackageId(Long lecturePackageId);
-//
-
-
-//    List<LecturePreviewDto> getLecturePreviewsByLectureId(int lectureId);
 }
 
 
