3fb7e63239720a5258d9b623623369bef35e82e9
qkskck326326 <160984009+qkskck326326@users.noreply.github.com>
2024-06-17 10:47:06 +0900

    2024-06-17 페이징 관련 시퀀스 수정

--- src/main/java/org/ict/intelligentclass/itnewsboard/jpa/repository/ItNewsBoardRepository.java
+++ src/main/java/org/ict/intelligentclass/itnewsboard/jpa/repository/ItNewsBoardRepository.java
@@ -3,9 +3,11 @@ package org.ict.intelligentclass.itnewsboard.jpa.repository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.ict.intelligentclass.itnewsboard.jpa.entity.ItNewsBoardEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface ItNewsBoardRepository extends JpaRepository<ItNewsBoardEntity, Long> {
-
+    Page<ItNewsBoardEntity> findByTitleContaining(String title, Pageable pageable);
 }//
