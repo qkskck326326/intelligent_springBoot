c57e41da0741fd9be8871da5e06ec433783ff08a
rudalsdl <lll0803lll@naver.com>
2024-06-17 18:08:40 +0900

    lecture 패키지입니다.

--- src/main/java/org/ict/intelligentclass/lecture/jpa/repository/LectureReadRepository.java
+++ src/main/java/org/ict/intelligentclass/lecture/jpa/repository/LectureReadRepository.java
@@ -3,10 +3,13 @@ package org.ict.intelligentclass.lecture.jpa.repository;
 import org.ict.intelligentclass.lecture.jpa.entity.LectureReadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface LectureReadRepository
     extends JpaRepository<LectureReadEntity, String> {
 
+    // 강의 읽음 처리
+    Optional<LectureReadEntity> findByLectureIdAndNickname(int lectureId, String nickname);
 }
 
--- src/main/java/org/ict/intelligentclass/lecture/jpa/repository/LectureRepository.java
+++ src/main/java/org/ict/intelligentclass/lecture/jpa/repository/LectureRepository.java
@@ -4,11 +4,14 @@ import org.ict.intelligentclass.lecture.jpa.entity.LectureEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface LectureRepository
     extends JpaRepository<LectureEntity, Integer> {
 
+    List<LectureEntity> findByLecturePackageId(Long lecturePackageId);
 }
 
 
