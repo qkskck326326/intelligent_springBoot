130a3ee999b5a1325e42cc451a1f14241e6a5b8c
doha12 <c3179923@gmail.com>
2024-06-21 18:17:17 +0900

    김도하 커밋

--- src/main/java/org/ict/intelligentclass/payment/jpa/repository/CouponRepositoy.java
+++ src/main/java/org/ict/intelligentclass/payment/jpa/repository/CouponRepositoy.java
@@ -3,5 +3,8 @@ package org.ict.intelligentclass.payment.jpa.repository;
 import org.ict.intelligentclass.payment.jpa.entity.CouponEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CouponRepositoy extends JpaRepository<CouponEntity, Long> {
+    List<CouponEntity> findByUserEmail(String userEmail);
 }
