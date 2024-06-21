dbef4a13ac1ee4a68e35c09c0bba6962fa93b172
chaelim <eoett88@gmail.com>
2024-06-21 11:50:15 +0900

    신고 리스트 조회 구현.

--- src/main/java/org/ict/intelligentclass/report/controller/ReportController.java
+++ src/main/java/org/ict/intelligentclass/report/controller/ReportController.java
@@ -2,14 +2,28 @@ package org.ict.intelligentclass.report.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.ict.intelligentclass.report.model.dto.ReportDto;
+import org.ict.intelligentclass.report.model.service.ReportService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @CrossOrigin
 @RestController
-@RequestMapping("/report")
+@RequestMapping("/reports")
 public class ReportController {
+
+    private final ReportService reportService;
+
+    @GetMapping
+    public ResponseEntity<List<ReportDto>> getReporListAll() {
+        List<ReportDto> report = reportService.getReportListAll();
+        return ResponseEntity.ok(report);
+    }
 }
--- src/main/java/org/ict/intelligentclass/report/jpa/repository/ReportRepository.java
+++ src/main/java/org/ict/intelligentclass/report/jpa/repository/ReportRepository.java
@@ -1,8 +1,10 @@
-//package org.ict.intelligentclass.report.jpa.repository;
-//
-//import org.ict.intelligentclass.report.jpa.entityv
-//        .ReportEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
-//}
+package org.ict.intelligentclass.report.jpa.repository;
+
+
+import org.ict.intelligentclass.report.jpa.entity.ReportEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
+
+
+}
