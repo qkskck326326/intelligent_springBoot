0df3211af090dc25a013fcd5e8a3289ab48d61b7
Kang_Tae_seok <goxotjr@naver.com>
2024-06-24 17:25:41 +0900

    태석

---
+++ src/main/java/org/ict/intelligentclass/security/config/MailConfig.java
@@ -0,0 +1,34 @@
+package org.ict.intelligentclass.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.naver.com");
+        mailSender.setPort(465);
+
+        mailSender.setUsername("officialintelliclass@naver.com");
+        mailSender.setPassword("7W99PXF8JF9H");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.smtp.ssl.trust", "smtp.naver.com");
+//        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
